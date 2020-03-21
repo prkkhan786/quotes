ac1fa15431ddb166e0fb5c7336d9bdfb40e0b74d
prkkhan786 <prkkhan1@gmail.com>
2020-03-21 11:27:27 +0530

    Updates

---
+++ precache-manifest.39198051eb21300a62e0c740d43fb0bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9b151a2c1e3ec3780b98ffae74024c",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "c8bd60fbdc1880bc464c",
+    "url": "/quotes/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "563b37dc8cfb078c16c7",
+    "url": "/quotes/static/css/main.e80ecbe8.chunk.css"
+  },
+  {
+    "revision": "c8bd60fbdc1880bc464c",
+    "url": "/quotes/static/js/2.82e71173.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/quotes/static/js/2.82e71173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "563b37dc8cfb078c16c7",
+    "url": "/quotes/static/js/main.ef2efe42.chunk.js"
+  },
+  {
+    "revision": "69c49785081a067a6ff9",
+    "url": "/quotes/static/js/runtime-main.9c799e86.js"
+  }
+]);
